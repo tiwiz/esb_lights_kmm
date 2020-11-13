b2368844e5bf2ca017eb04274025a771865247ed
tiwiz <roberto.orgiu@gmail.com>
2020-11-13 09:25:16 +0100

    Upgrade build to latest AS Canary

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,18 +1,57 @@
 import SwiftUI
 import shared
 
-func greet() -> String {
-    return Greeting().greeting()
-}
-
 struct ContentView: View {
+    @ObservedObject private(set) var viewModel: ViewModel
+    
     var body: some View {
-        Text(greet())
+        NavigationView {
+            listView()
+                .navigationBarTitle("ESB Lights")
+                .navigationBarItems(trailing:
+                                        Button("Reload") {
+                                            self.viewModel.loadLights()
+                                        })
+        }
+    }
+    
+    private func listView() -> AnyView {
+        switch viewModel.lights {
+        case .loading:
+            return AnyView(Text("Loading...").multilineTextAlignment(.center))
+        case .result(let lodableLights):
+            return AnyView(LightsView(lights: lodableLights))
+        case .error(let description):
+            return AnyView(Text(description).multilineTextAlignment(.center))
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+extension ContentView {
+    enum LoadableLights {
+        case loading
+        case result(Lights)
+        case error(String)
+    }
+    
+    class ViewModel: ObservableObject {
+        let repo : LightRepository
+        @Published var lights = LoadableLights.loading
+        
+        init(repo: LightRepository){
+            self.repo = repo
+            self.loadLights()
+        }
+        
+        func loadLights() {
+            self.lights = .loading
+            repo.getLights(completionHandler: { lights, error in
+                if let lights = lights {
+                    self.lights = .result(lights)
+                } else {
+                    self.lights = .error(error?.localizedDescription ?? "error")
+                }
+            })
+        }
     }
 }
